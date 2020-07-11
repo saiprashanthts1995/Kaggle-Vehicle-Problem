70fcbaa9261434196d2bc020413da24a117066fc
Sai Prashanth T S <saiprashanthts@gmail.com>
2020-07-11 20:19:07 +0530

    added basic plots

--- app.py
+++ app.py
@@ -1,16 +1,23 @@
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
 
 # Reading the file as df
 df = pd.read_csv('car data.csv')
 
+
 # Df read the 5 rows
 print(df.head())
+
 
 # DF find the no of columns and rows
 print(df.shape)
 
+
 # List of Columns
 print(list(df.columns))
+
 
 # Unique Values for Categorical columns
 print(df['Seller_Type'].unique())
@@ -19,28 +26,21 @@ print(df['Owner'].unique())
 print(df['Fuel_Type'].unique())
 
 
-
 # Find the missing values
 print(df.isnull().sum())
 
 
 # Describe the data frame
 print(df.describe())
-print(df.describe(
-    include=['object']
-))
+print(df.describe(include=['object']))
 print(df.info())
 
 
 # Drooping the Car name as it will not have bg impact
-
 final_data = df[['Year', 'Selling_Price', 'Present_Price', 'Kms_Driven', 'Fuel_Type', 'Seller_Type', 'Transmission', 'Owner']]
-
 print(final_data.head())
 
-
 # Creating new feature
-
 final_data['current_year'] = 2020
 final_data['no_of_years'] = final_data['current_year'] - final_data['Year']
 print(final_data.head())
@@ -52,5 +52,19 @@ print(final_data.head())
 
 
 # convert categorical variables into numeric columns
+# drop_first was set as True so that dummy variable trap could be avoided
 final_data = pd.get_dummies(final_data, drop_first=True)
 print(final_data.head())
+
+# Finding the correlation
+print(final_data.corr())
+
+# Correlation plot
+# print(sns.pairplot(final_data))
+# print(plt.show())
+
+# Plotting heat map
+corr_mat = final_data.corr()
+print(corr_mat)
+top_corr = corr_mat.index
+plt.figure(figsize=(20, 20), )
